83ad7441fddf47cbb8828f469cee4d49a01ec089
Tony Tomov <tony@trirand.com>
2009-05-01 17:17:25 +0300

    Changed the path to language file so that it meets the new dir structure

--- jquery.jqGrid.js
+++ jquery.jqGrid.js
@@ -7,7 +7,7 @@ function jqGridInclude()
     var combinedIncludeURL = "combine.php?type=javascript&files=";
     var minver = false;
     var modules = [
-        { include: true, incfile:'grid.locale-en.js',minfile: 'min/grid.locale-en-min.js'}, // jqGrid translation
+        { include: true, incfile:'i18n/grid.locale-en.js',minfile: 'i18n/grid.locale-en-min.js'}, // jqGrid translation
         { include: true, incfile:'grid.base.js',minfile: 'min/grid.base-min.js'}, // jqGrid base
         { include: true, incfile:'grid.common.js',minfile: 'min/grid.common-min.js' }, // jqGrid common for editing
         { include: true, incfile:'grid.formedit.js',minfile: 'min/grid.formedit-min.js' }, // jqGrid Form editing
