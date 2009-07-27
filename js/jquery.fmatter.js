6a6328df022c0f64f126af75d796de8b47df6d1f
straps <fstraps+github@gmail.com>
2009-07-27 15:01:59 +0200

    Completed the possibility to use a custom formatter and specify the unformat function as a method of the formatter itself

--- js/jquery.fmatter.js
+++ js/jquery.fmatter.js
@@ -330,17 +330,20 @@
 		// specific for jqGrid only
 		var ret, formatType = options.colModel.formatter,
 		op =options.colModel.formatoptions || {},
-		unformatFunc = options.colModel.unformat;
+		unformatFunc = options.colModel.unformat||($.fn.fmatter[formatType] && $.fn.fmatter[formatType].unformat);
+        
 		if(unformatFunc !== 'undefined' && isFunction(unformatFunc) ) {
 			ret = unformatFunc(cellval, options);
 		} else if(formatType !== 'undefined' && isString(formatType) ) {
 			var opts = $.jgrid.formatter || {}, stripTag;
 			switch(formatType) {
+                /*
 				case 'link' :
 				case 'showlink' :
 				case 'email' :
 					ret= $(cellval).text();
 					break;
+                */
 				case 'integer' :
 					op = $.extend({},opts.integer,op);
 					stripTag = eval("/"+op.thousandsSeparator+"/g");
@@ -360,6 +363,9 @@
 					var cbv = (options.colModel.editoptions) ? options.colModel.editoptions.value.split(":") : ["Yes","No"];
 					ret = $('input',cellval).attr("checked") ? cbv[0] : cbv[1];
 					break;
+                default:
+                    ret= $(cellval).text();
+                    break;
 			}
 		}
 		return ret ? ret : cnt===true ? $(cellval).text() : $.htmlDecode($(cellval).html());
