e23ddfb7671d9ab55021d98006b65b5dee31b9f7
tomovtony <tony@trirand.com>
2009-08-04 15:57:38 +0300

    Upadte for french translation

--- js/i18n/grid.locale-fr.js
+++ js/i18n/grid.locale-fr.js
@@ -9,81 +9,81 @@
 **/
 $.jgrid = {
 	defaults : {
-		recordtext: "View {0} - {1} of {2}",
-	    emptyrecords: "No records to view",
+		recordtext: "Enregistrements {0} - {1} sur {2}",
+		emptyrecords: "Aucun enregistrement a afficher",
 		loadtext: "Chargement...",
-		pgtext : "Page {0} of {1}"
+		pgtext : "Page {0} sur {1}"
 	},
 	search : {
-	    caption: "Recherche...",
-	    Find: "Chercher",
-	    Reset: "Annuler",
-	    odata : ['equal', 'not equal', 'less', 'less or equal','greater','greater or equal', 'begins with','does not begin with','is in','is not in','ends with','does not end with','contains','does not contain'],
-	    groupOps: [	{ op: "AND", text: "all" },	{ op: "OR",  text: "any" }	],
-		matchText: " match",
-		rulesText: " rules"
+		caption: "Recherche...",
+		Find: "Chercher",
+		Reset: "Annuler",
+		odata : ['egal', 'différent', 'inférieur', 'inférieur ou égal','supérieur','supérieur ou égal', 'commence par','ne commence pas par','est dans',"n'est pas dans",'fini par','ne fini pas par','contient','ne contient pas'],
+		groupOps: [	{ op: "AND", text: "tous" },	{ op: "OR",  text: "aucun" }	],
+		matchText: " correspondance",
+		rulesText: " règles"
 	},
 	edit : {
-	    addCaption: "Ajouter",
-	    editCaption: "Editer",
-	    bSubmit: "Valider",
-	    bCancel: "Annuler",
+		addCaption: "Ajouter",
+		editCaption: "Editer",
+		bSubmit: "Valider",
+		bCancel: "Annuler",
 		bClose: "Fermer",
-		saveData: "Data has been changed! Save changes?",
-		bYes : "Yes",
-		bNo : "No",
-		bExit : "Cancel",
-	    msg: {
-	        required:"Champ obligatoire",
-	        number:"Saisissez un nombre valide",
-	        minValue:"La valeur doit être supérieure ou égal à 0 ",
-	        maxValue:"La valeur doit être inférieure ou égal à 0",
-	        email: "n'est pas un email valide",
-	        integer: "Saisissez un entier valide",
-			url: "is not a valid URL. Prefix required ('http://' or 'https://')"
+		saveData: "Les données ont changé ! Enregistrer les modifications ?",
+		bYes: "Oui",
+		bNo: "Non",
+		bExit: "Annuler",
+		msg: {
+			required: "Champ obligatoire",
+			number: "Saisissez un nombre correct",
+			minValue: "La valeur doit être supérieure ou égal à 0 ",
+			maxValue: "La valeur doit être inférieure ou égal à 0",
+			email: "n'est pas un email correct",
+			integer: "Saisissez un entier correct",
+			url: "n'est pas une adresse correcte. Préfixe requis ('http://' or 'https://')"
 		}
 	},
 	view : {
-	    caption: "View Record",
-	    bClose: "Close"
+		caption: "Voir les enregistrement",
+		bClose: "Fermer"
 	},
 	del : {
-	    caption: "Supprimer",
-	    msg: "Supprimer les enregistrements sélectionnés ?",
-	    bSubmit: "Supprimer",
-	    bCancel: "Annuler"
+		caption: "Supprimer",
+		msg: "Supprimer les enregistrements sélectionnés ?",
+		bSubmit: "Supprimer",
+		bCancel: "Annuler"
 	},
 	nav : {
 		edittext: " ",
-	    edittitle: "Editer la ligne sélectionnée",
+		edittitle: "Editer la ligne sélectionnée",
 		addtext:" ",
-	    addtitle: "Ajouter une ligne",
-	    deltext: " ",
-	    deltitle: "Supprimer la ligne sélectionnée",
-	    searchtext: " ",
-	    searchtitle: "Chercher un enregistrement",
-	    refreshtext: "",
-	    refreshtitle: "Recharger le tableau",
-	    alertcap: "Avertissement",
-	    alerttext: "Veuillez sélectionner une ligne",
+		addtitle: "Ajouter une ligne",
+		deltext: " ",
+		deltitle: "Supprimer la ligne sélectionnée",
+		searchtext: " ",
+		searchtitle: "Chercher un enregistrement",
+		refreshtext: "",
+		refreshtitle: "Recharger le tableau",
+		alertcap: "Avertissement",
+		alerttext: "Veuillez sélectionner une ligne",
 		viewtext: "",
-		viewtitle: "View selected row"
+		viewtitle: "Afficher la ligne sélectionnée"
 	},
 	col : {
-	    caption: "Afficher/Masquer les colonnes",
-	    bSubmit: "Valider",
-	    bCancel: "Annuler"	
+		caption: "Afficher/Masquer les colonnes",
+		bSubmit: "Valider",
+		bCancel: "Annuler"
 	},
 	errors : {
 		errcap : "Erreur",
-		nourl : "Aucune url paramétrée",
+		nourl : "Aucune adresse paramétrée",
 		norecords: "Aucun enregistrement à traiter",
 		model : "Nombre de titres (colNames) <> Nombre de données (colModel)!"
 	},
 	formatter : {
 		integer : {thousandsSeparator: " ", defaultValue: '0'},
-		number : {decimalSeparator:".", thousandsSeparator: " ", decimalPlaces: 2, defaultValue: '0.00'},
-		currency : {decimalSeparator:".", thousandsSeparator: " ", decimalPlaces: 2, prefix: "", suffix:"", defaultValue: '0.00'},
+		number : {decimalSeparator:",", thousandsSeparator: " ", decimalPlaces: 2, defaultValue: '0,00'},
+		currency : {decimalSeparator:",", thousandsSeparator: " ", decimalPlaces: 2, prefix: "", suffix:"", defaultValue: '0,00'},
 		date : {
 			dayNames:   [
 				"Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam",
@@ -98,24 +98,24 @@ $.jgrid = {
 			srcformat: 'Y-m-d',
 			newformat: 'd/m/Y',
 			masks : {
-		        ISO8601Long:"Y-m-d H:i:s",
-		        ISO8601Short:"Y-m-d",
-		        ShortDate: "n/j/Y",
-		        LongDate: "l, F d, Y",
-		        FullDateTime: "l, F d, Y g:i:s A",
-		        MonthDay: "F d",
-		        ShortTime: "g:i A",
-		        LongTime: "g:i:s A",
-		        SortableDateTime: "Y-m-d\\TH:i:s",
-		        UniversalSortableDateTime: "Y-m-d H:i:sO",
-		        YearMonth: "F, Y"
-		    },
-		    reformatAfterEdit : false
+				ISO8601Long:"Y-m-d H:i:s",
+				ISO8601Short:"Y-m-d",
+				ShortDate: "n/j/Y",
+				LongDate: "l, F d, Y",
+				FullDateTime: "l, F d, Y g:i:s A",
+				MonthDay: "F d",
+				ShortTime: "g:i A",
+				LongTime: "g:i:s A",
+				SortableDateTime: "Y-m-d\\TH:i:s",
+				UniversalSortableDateTime: "Y-m-d H:i:sO",
+				YearMonth: "F, Y"
+			},
+			reformatAfterEdit : false
 		},
 		baseLinkUrl: '',
 		showAction: '',
-	    target: '',
-	    checkbox : {disabled:true},
+		target: '',
+		checkbox : {disabled:true},
 		idName : 'id'
 	}
 };
