7302925a72226328269b502b6c08bf41fbf6b526
tomovtony <tony@trirand.com>
2009-09-12 18:36:49 +0300

    Moving remapColumns in base module.

--- js/grid.base.js
+++ js/grid.base.js
@@ -1740,6 +1740,68 @@ $.jgrid.extend({
 	showCol : function(colname) {
 		return this.each(function(){$(this).jqGrid("ShowHideCol",colname,"");});
 	},
+	remapColumns : function(permutation, updateCells, keepHeader)
+    {
+        /*
+          After remapColumns, the column with index i, is the column
+          that used to have index permutation[i].
+
+          updateCells can be set to false as an optimization if you're going to refetch
+          the data anyway
+
+          keepHeader is really just to optimize "sortable". After sortable runs,
+          the headers have already been re-ordered. Rather than put the header back
+          where it started, and then move it again, we can simply leave it where
+          sortable left it.
+          */
+        function resortArray(a) {
+            var ac;
+            if (a.length) {
+                ac = $.makeArray(a);
+            } else {
+                ac = $.extend({}, a);
+            }
+            $.each(permutation, function(i) {
+                a[i] = ac[this];
+            });
+        }
+
+        var ts = this.get(0);
+        function resortRows(parent, clobj) {
+            $(">tr"+(clobj||""), parent).each(function() {
+                var row = this;
+                var elems = $.makeArray(row.cells);
+                $.each(permutation, function() {
+                    var e = elems[this];
+                    if (e) {
+                        row.appendChild(e);
+                    }
+                });
+            });
+        }
+
+        resortArray(ts.p.colModel);
+        resortArray(ts.p.colNames);
+        resortArray(ts.grid.headers);
+
+        resortRows($("thead:first", ts.grid.hDiv), keepHeader && ":not(.ui-jqgrid-labels)");
+
+        if (updateCells) {
+            resortRows($("tbody:first", ts.grid.bDiv), ".jqgrow");
+            if (ts.p.footerrow) {
+                resortRows($("tbody:first", ts.grid.sDiv));
+            }
+        }
+
+        if (ts.p.remapColumns) {
+            if (!ts.p.remapColumns.length)
+                ts.p.remapColumns = $.makeArray(permutation);
+            else
+                resortArray(ts.p.remapColumns);
+        }
+        ts.p.lastsort = $.inArray(ts.p.lastsort, permutation);
+        if(ts.p.treeGrid) ts.p.expColInd = $.inArray(ts.p.expColInd, permutation);
+    },
 	setGridWidth : function(nwidth, shrink) {
 		return this.each(function(){
 			var $t = this, cw,
