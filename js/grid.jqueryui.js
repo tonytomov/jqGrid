3e2acea014f8caa712a8043144613bd2ed1d2855
tomovtony <tony@trirand.com>
2009-09-12 17:19:58 +0300

    Spelling error

--- js/grid.jqueryui.js
+++ js/grid.jqueryui.js
@@ -2,7 +2,7 @@
 /*
 **
  * jqGrid addons using jQuery UI 
- * Author: Mark Willams
+ * Author: Mark Williams
  * Dual licensed under the MIT and GPL licenses:
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
