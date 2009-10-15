86ec2c3ef04e10b03b659a7ea8e83314382a3d1a
tomovtony <tony@trirand.com>
2009-10-15 12:22:56 +0300

    We forget again the serializeSubGridData in the ajax configuration

--- js/grid.subgrid.js
+++ js/grid.subgrid.js
@@ -101,7 +101,7 @@ addSubGrid : function(t,pos) {
 						type:ts.p.mtype,
 						url: ts.p.subGridUrl,
 						dataType:ts.p.subgridtype,
-						data: dp,
+						data: $.isFunction(ts.p.serializeSubGridData)? ts.p.serializeSubGridData(dp) : dp,
 						complete: function(sxml) {
 							if(ts.p.subgridtype == "xml")
 								subGridXml(sxml.responseXML, sid);
