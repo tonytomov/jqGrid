d7d2b82dc3a06c1d74658e8df8a1114f795def64
Tony Tomov <tony@trirand.com>
2009-11-10 14:23:03 +0200

    Fixing the xmlJsonClass.toJson function not to modify the input object. This causes bad export and destroys the jqGrid object when used in FireFox and Linux and Mac.

--- js/JsonXml.js
+++ js/JsonXml.js
@@ -224,11 +224,11 @@ var xmlJsonClass = {
 			json += (name ? ":[]" : "[]");
 		}
 		else if (o instanceof Array) {
-			var n, i;
+			var n, i, ar=[];
 			for (i = 0, n = o.length; i < n; i += 1) {
-				o[i] = this.toJson(o[i], "", ind + "\t");
+				ar[i] = this.toJson(o[i], "", ind + "\t");
 			}
-			json += (name ? ":[" : "[") + (o.length > 1 ? ("\n" + ind + "\t" + o.join(",\n" + ind + "\t") + "\n" + ind) : o.join("")) + "]";
+			json += (name ? ":[" : "[") + (ar.length > 1 ? ("\n" + ind + "\t" + ar.join(",\n" + ind + "\t") + "\n" + ind) : ar.join("")) + "]";
 		}
 		else if (o === null) {
 			json += (name && ":") + "null";
@@ -244,10 +244,10 @@ var xmlJsonClass = {
 		else if (typeof(o) === "string") {
 			var objRegExp  = /(^-?\d+\.?\d*$)/;
 			var FuncTest = /function/i;
-			o = o.toString();
-			if (objRegExp.test(o) || FuncTest.test(o) || o==="false" || o==="true") {
+			var os = o.toString();
+			if (objRegExp.test(os) || FuncTest.test(os) || os==="false" || os==="true") {
 				// int or float
-				json += (name && ":") + o;
+				json += (name && ":") + os;
 			} 
 			else {
 				json += (name && ":") + "\"" + o + "\"";
