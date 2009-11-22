ed81576b1edc86c3b15141235be4ebe268353709
Tony Tomov <tony@trirand.com>
2009-11-22 14:42:41 +0200

    Update for Brazilian-Portuguese language file

--- js/i18n/grid.locale-pt-br.js
+++ js/i18n/grid.locale-pt-br.js
@@ -1,18 +1,18 @@
 ;(function($){
 /**
  * jqGrid Brazilian-Portuguese Translation
- * Junior Gobira juniousbr@gmail.com
- * http://jnsa.com.br
+ * Sergio Righi sergio.righi@gmail.com
+ * http://curve.com.br
  * Dual licensed under the MIT and GPL licenses:
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
 **/
 $.jgrid = {
 	defaults : {
-		recordtext: "View {0} - {1} of {2}",
-	    emptyrecords: "No records to view",
+		recordtext: "Ver {0} - {1} of {2}",
+	    emptyrecords: "Nenhum registro para visualizar",
 		loadtext: "Carregando...",
-		pgtext : "Page {0} of {1}"
+		pgtext : "Página {0} de {1}"
 	},
 	search : {
 	    caption: "Procurar...",
@@ -20,8 +20,8 @@ $.jgrid = {
 	    Reset: "Resetar",
 	    odata : ['equal', 'not equal', 'less', 'less or equal','greater','greater or equal', 'begins with','does not begin with','is in','is not in','ends with','does not end with','contains','does not contain'],
 	    groupOps: [	{ op: "AND", text: "all" },	{ op: "OR",  text: "any" }	],
-		matchText: " match",
-		rulesText: " rules"
+		matchText: " iguala",
+		rulesText: " regras"
 	},
 	edit : {
 	    addCaption: "Incluir",
@@ -29,33 +29,33 @@ $.jgrid = {
 	    bSubmit: "Enviar",
 	    bCancel: "Cancelar",
 		bClose: "Fechar",
-		saveData: "Data has been changed! Save changes?",
-		bYes : "Yes",
-		bNo : "No",
-		bExit : "Cancel",
+		saveData: "Os dados foram alterados! Salvar alterações?",
+		bYes : "Sim",
+		bNo : "Não",
+		bExit : "Cancelar",
 	    msg: {
-	        required:"Campo � requerido",
-	        number:"Por favor, informe um n�mero v�lido",
+	        required:"Campo obrigatório",
+	        number:"Por favor, informe um número válido",
 	        minValue:"valor deve ser igual ou maior que ",
 	        maxValue:"valor deve ser menor ou igual a",
-	        email: "este e-mail n�o � v�lido",
+	        email: "este e-mail não é válido",
 	        integer: "Por favor, informe um valor inteiro",
-			date: "Please, enter valid date value",
-			url: "is not a valid URL. Prefix required ('http://' or 'https://')",
-			nodefined : " is not defined!",
-			novalue : " return value is required!",
-			customarray : "Custom function should return array!",
-			customfcheck : "Custom function should be present in case of custom checking!"
+			date: "Por favor, informe uma data válida",
+			url: "não é uma URL válida. Prefixo obrigatório ('http://' or 'https://')",
+			nodefined : " não está definido!",
+			novalue : " um valor de retorno é obrigatório!",
+			customarray : "Função customizada deve retornar um array!",
+			customfcheck : "Função customizada deve estar presente em caso de validação customizada!"
 		}
 	},
 	view : {
-	    caption: "View Record",
-	    bClose: "Close"
+	    caption: "Ver Registro",
+	    bClose: "Fechar"
 	},
 	del : {
-    caption: "Delete",
-	    msg: "Deletar registros selecionado(s)?",
-	    bSubmit: "Delete",
+    caption: "Apagar",
+	    msg: "Apagar registros selecionado(s)?",
+	    bSubmit: "Apagar",
 	    bCancel: "Cancelar"
 	},
 	nav : {
@@ -64,7 +64,7 @@ $.jgrid = {
 		addtext:" ",
 	    addtitle: "Incluir novo registro",
 	    deltext: " ",
-	    deltitle: "Deletar registro selecionado",
+	    deltitle: "Apagar registro selecionado",
 	    searchtext: " ",
 	    searchtitle: "Procurar registros",
 	    refreshtext: "",
@@ -72,7 +72,7 @@ $.jgrid = {
 	    alertcap: "Aviso",
 	    alerttext: "Por favor, selecione um registro",
 		viewtext: "",
-		viewtitle: "View selected row"
+		viewtitle: "Ver linha selecionada"
 	},
 	col : {
 	    caption: "Mostrar/Esconder Colunas",
@@ -83,23 +83,23 @@ $.jgrid = {
 		errcap : "Erro",
 		nourl : "Nenhuma URL defenida",
 		norecords: "Sem registros para exibir",
-	    model : "Length of colNames <> colModel!"
+	    model : "Comprimento de colNames <> colModel!"
 	},
 	formatter : {
 		integer : {thousandsSeparator: " ", defaultValue: '0'},
-		number : {decimalSeparator:".", thousandsSeparator: " ", decimalPlaces: 2, defaultValue: '0.00'},
-		currency : {decimalSeparator:".", thousandsSeparator: " ", decimalPlaces: 2, prefix: "", suffix:"", defaultValue: '0.00'},
+		number : {decimalSeparator:",", thousandsSeparator: " ", decimalPlaces: 2, defaultValue: '0,00'},
+		currency : {decimalSeparator:",", thousandsSeparator: ".", decimalPlaces: 2, prefix: "R$ ", suffix:"", defaultValue: '0,00'},
 		date : {
 			dayNames:   [
-				"Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat",
-				"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+				"Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb",
+				"Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"
 			],
 			monthNames: [
-				"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
-				"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+				"Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez",
+				"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
 			],
 			AmPm : ["am","pm","AM","PM"],
-			S: function (j) {return j < 11 || j > 13 ? ['st', 'nd', 'rd', 'th'][Math.min((j - 1) % 10, 3)] : 'th'},
+			S: function (j) {return j < 11 || j > 13 ? ['º', 'º', 'º', 'º'][Math.min((j - 1) % 10, 3)] : 'º'},
 			srcformat: 'Y-m-d',
 			newformat: 'd/m/Y',
 			masks : {
