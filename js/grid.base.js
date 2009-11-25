0ab213e41610d814b7c26929602576e393684be9
Tony Tomov <tony@trirand.com>
2009-11-25 16:52:59 +0200

    Little late, but ... Publishing 3.6.1

--- js/grid.base.js
+++ js/grid.base.js
@@ -1,11 +1,11 @@
 ;(function ($) {
 /*
- * jqGrid  3.6 - jQuery Grid
+ * jqGrid  3.6.1 - jQuery Grid
  * Copyright (c) 2008, Tony Tomov, tony@trirand.com
  * Dual licensed under the MIT and GPL licenses
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
- * Date: 2009-11-08
+ * Date: 2009-11-23
  */
 $.jgrid = $.jgrid || {};
 $.extend($.jgrid,{
