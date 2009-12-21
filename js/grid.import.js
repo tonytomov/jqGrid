bc9768dc4c0263e2c5a08e3bd0ae767ef9b85660
Tony Tomov <tony@trirand.com>
2009-12-21 21:05:16 +0200

    Added primary excel Export method to be compatible with  jqGrid PHP framework. Also Export to excel work ok from PHP

--- js/grid.import.js
+++ js/grid.import.js
@@ -163,6 +163,23 @@
                 }
             });
             return ret;
+        },
+        excelExport : function(o) {
+            o = $.extend({
+                exptype : "remote",
+                url : null,
+                exportOptions : {}
+            }, o || {});
+            return this.each(function(){
+                $t = this;
+                if(!this.grid) { return;}
+                if(o.exptype == "remote") {
+                    var pdata = $.extend({},this.p.postData);
+                    pdata.oper = "excel";
+                    var params = jQuery.param(pdata);
+                    window.location = o.url+"?"+params;
+                }
+            });
         }
     });
 })(jQuery);
