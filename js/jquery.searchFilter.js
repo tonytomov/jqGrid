b644adf3f30d5f40f76c35f9349c7cf9f4eb17dc
Tony Tomov <tony@trirand.com>
2009-12-28 22:56:24 +0200

    Remove a special char at the beginning of the file

--- js/jquery.searchFilter.js
+++ js/jquery.searchFilter.js
@@ -1,4 +1,4 @@
-﻿/* Plugin:      searchFilter v1.2.9
+/* Plugin:      searchFilter v1.2.9
  * Author:      Kasey Speakman (kasey@cornerspeed.com)
  * License:     Dual Licensed, MIT and GPL v2 (http://www.gnu.org/copyleft/gpl.html)
  *
