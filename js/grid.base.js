535db87700c1598c0d96d263082b1c6a7113189d
Tony Tomov <tony@trirand.com>
2010-05-13 16:14:22 +0300

    Moving parseDate to be visible in all modules.

--- js/grid.base.js
+++ js/grid.base.js
@@ -63,6 +63,51 @@ $.extend($.jgrid,{
 		    JSON.parse(js) :
 		    eval('(' + js + ')');
 	},
+	parseDate : function(format, date) {
+		var tsp = {m : 1, d : 1, y : 1970, h : 0, i : 0, s : 0},k,hl,dM;
+		date = $.trim(date);
+		if(date){
+			date = date.split(/[\\\/:_;.\t\T\s-]/);
+			format = format.split(/[\\\/:_;.\t\T\s-]/);
+			var dfmt  = $.jgrid.formatter.date.monthNames;
+			var afmt  = $.jgrid.formatter.date.AmPm;
+			var h12to24 = function(ampm, h){
+				if (ampm === 0){ if (h == 12) { h = 0;} }
+				          else { if (h != 12) { h += 12; } }
+				return h;
+			}; 
+			for(k=0,hl=format.length;k<hl;k++){
+				if(format[k] == 'M') {
+					dM = $.inArray(date[k],dfmt);
+					if(dM !== -1 && dM < 12){date[k] = dM+1;}
+				}
+				if(format[k] == 'F') {
+					dM = $.inArray(date[k],dfmt);
+					if(dM !== -1 && dM > 11){date[k] = dM+1-12;}
+				}
+				if(format[k] == 'a') {
+					dM = $.inArray(date[k],afmt);
+					if(dM !== -1 && dM < 2 && date[k] == afmt[dM]){
+						date[k] = dM;
+						tsp.h = h12to24(date[k], tsp.h);
+					}
+				}
+				if(format[k] == 'A') {
+					dM = $.inArray(date[k],afmt);
+					if(dM !== -1 && dM > 1 && date[k] == afmt[dM]){
+						date[k] = dM-2;
+						tsp.h = h12to24(date[k], tsp.h);
+					}
+				}
+				tsp[format[k].toLowerCase()] = parseInt(date[k],10);
+			}
+			tsp.m = parseInt(tsp.m,10)-1;
+			var ty = tsp.y;
+			if (ty >= 70 && ty <= 99) {tsp.y = 1900+tsp.y;}
+			else if (ty >=0 && ty <=69) {tsp.y= 2000+tsp.y;}
+		}
+		return new Date(tsp.y, tsp.m, tsp.d, tsp.h, tsp.i, tsp.s,0);
+	},
 	jqID : function(sid){
 		sid = sid + "";
 		return sid.replace(/([\.\:\[\]])/g,"\\$1");
@@ -960,51 +1005,6 @@ $.fn.jqGrid = function( pin ) {
 				});
 			}
 			ts.grid.bDiv.scrollTop = 0;
-		},
-		parseDate = function(format, date) {
-			var tsp = {m : 1, d : 1, y : 1970, h : 0, i : 0, s : 0},k,hl,dM;
-			date = $.trim(date);
-			if(date){
-				date = date.split(/[\\\/:_;.\t\T\s-]/);
-				format = format.split(/[\\\/:_;.\t\T\s-]/);
-				var dfmt  = $.jgrid.formatter.date.monthNames;
-				var afmt  = $.jgrid.formatter.date.AmPm;
-				var h12to24 = function(ampm, h){
-					if (ampm === 0){ if (h == 12) { h = 0;} }
-					          else { if (h != 12) { h += 12; } }
-					return h;
-				}; 
-				for(k=0,hl=format.length;k<hl;k++){
-					if(format[k] == 'M') {
-						dM = $.inArray(date[k],dfmt);
-						if(dM !== -1 && dM < 12){date[k] = dM+1;}
-					}
-					if(format[k] == 'F') {
-						dM = $.inArray(date[k],dfmt);
-						if(dM !== -1 && dM > 11){date[k] = dM+1-12;}
-					}
-					if(format[k] == 'a') {
-						dM = $.inArray(date[k],afmt);
-						if(dM !== -1 && dM < 2 && date[k] == afmt[dM]){
-							date[k] = dM;
-							tsp.h = h12to24(date[k], tsp.h);
-						}
-					}
-					if(format[k] == 'A') {
-						dM = $.inArray(date[k],afmt);
-						if(dM !== -1 && dM > 1 && date[k] == afmt[dM]){
-							date[k] = dM-2;
-							tsp.h = h12to24(date[k], tsp.h);
-						}
-					}
-					tsp[format[k].toLowerCase()] = parseInt(date[k],10);
-				}
-				tsp.m = parseInt(tsp.m,10)-1;
-				var ty = tsp.y;
-				if (ty >= 70 && ty <= 99) {tsp.y = 1900+tsp.y;}
-				else if (ty >=0 && ty <=69) {tsp.y= 2000+tsp.y;}
-			}
-			return new Date(tsp.y, tsp.m, tsp.d, tsp.h, tsp.i, tsp.s,0);
 		},
 		setPager = function (pgid, tp){
 			var sep = "<td class='ui-pg-button ui-state-disabled' style='width:4px;'><span class='ui-separator'></span></td>",
