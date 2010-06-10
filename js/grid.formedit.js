8fda0819d920322c96057128927e16614d32e78f
Tony Tomov <tony@trirand.com>
2010-06-10 11:25:04 +0300

    Fix for getting the name as attribute when the element is not a input one.

--- js/grid.formedit.js
+++ js/grid.formedit.js
@@ -644,7 +644,7 @@ $.jgrid.extend({
 				$(".FormElement", "#"+frmtb).each(function(i) {
 					var celm = $(".customelement", this);
 					if (celm.length) {
-						var  elem = celm[0], nm = elem.name;
+						var  elem = celm[0], nm = $(elem).attr('name');
 						$.each($t.p.colModel, function(i,n){
 							if(this.name == nm && this.editoptions && $.isFunction(this.editoptions.custom_value)) {
 								try {
