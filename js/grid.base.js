234120c9a5e108794783613e0c76bfb63b9d92d4
Tony Tomov <tony@trirand.com>
2010-06-15 13:53:04 +0300

    Publishing 3.7

--- js/grid.base.js
+++ js/grid.base.js
@@ -1,11 +1,11 @@
 ;(function ($) {
 /*
- * jqGrid  3.7 beta- jQuery Grid
+ * jqGrid  3.7 - jQuery Grid
  * Copyright (c) 2008, Tony Tomov, tony@trirand.com
  * Dual licensed under the MIT and GPL licenses
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl-2.0.html
- * Date: 2010-05-24 
+ * Date: 2010-06-15 
  */
 $.jgrid = $.jgrid || {};
 $.extend($.jgrid,{
