ef8fd3ad3e677f7a5ed08f002e884f36c3404e02
Tony Tomov <tony@trirand.com>
2011-01-20 08:16:04 +0200

    Update for Finnish translation

--- js/i18n/grid.locale-fi.js
+++ js/i18n/grid.locale-fi.js
@@ -8,62 +8,60 @@
  * http://www.gnu.org/licenses/gpl.html
 **/
 $.jgrid = {
-	defaults : {
-		//recordtext: "N&auml;yt&auml; {0} - {1} / {2}",
-		recordtext: " {0}-{1}/{2}",
-	        emptyrecords: "Ei n&auml;ytett&auml;vi&auml;",
+	defaults: {
+		recordtext: "Rivit {0} - {1} / {2}",
+	    emptyrecords: "Ei n&auml;ytett&auml;vi&auml;",
 		loadtext: "Haetaan...",
-		//pgtext : "Sivu {0} / {1}"
-		pgtext : "{0}/{1}"
+		pgtext: "Sivu {0} / {1}"
 	},
-	search : {
+	search: {
 	    caption: "Etsi...",
 	    Find: "Etsi",
 	    Reset: "Tyhj&auml;&auml;",
-	    odata : ['=', '<>', '<', '<=','>','>=', 'alkaa','ei ala','joukossa','ei joukossa ','loppuu','ei lopu','sis&auml;lt&auml;&auml;','ei sis&auml;ll&auml;'],
-	    groupOps: [	{ op: "JA", text: "kaikki" },	{ op: "TAI",  text: "mik&auml; tahansa" }	],
-		matchText: " match",
-		rulesText: " rules"
+	    odata: ['=', '<>', '<', '<=','>','>=', 'alkaa','ei ala','joukossa','ei joukossa','loppuu','ei lopu','sis&auml;lt&auml;&auml;','ei sis&auml;ll&auml;'],
+	    groupOps: [	{ op: "AND", text: "kaikki" }, { op: "OR", text: "mik&auml; tahansa" }	],
+		matchText: "&nbsp;&nbsp;t&auml;yt&auml; ehdot:",
+		rulesText: ""
 	},
-	edit : {
+	edit: {
 	    addCaption: "Uusi rivi",
-	    editCaption: "Muokkaa rivi",
+	    editCaption: "Muokkaa rivi&auml;",
 	    bSubmit: "OK",
 	    bCancel: "Peru",
 		bClose: "Sulje",
-		saveData: "Tietoja muutettu! Tallenetaanko?",
-		bYes : "K",
-		bNo : "E",
-		bExit : "Peru",
+		saveData: "Tietoja muutettu! Tallennetaanko?",
+		bYes: "K",
+		bNo: "E",
+		bExit: "Peru",
 	    msg: {
-	        required:"pakollinen",
-	        number:"Anna kelvollinen nro",
-	        minValue:"arvo oltava >= ",
-	        maxValue:"arvo oltava  <= ",
+	        required: "pakollinen",
+	        number: "Anna kelvollinen nro",
+	        minValue: "arvo oltava >= ",
+	        maxValue: "arvo oltava <= ",
 	        email: "virheellinen sposti ",
 	        integer: "Anna kelvollinen kokonaisluku",
 			date: "Anna kelvollinen pvm",
 			url: "Ei ole sopiva linkki(URL). Alku oltava ('http://' tai 'https://')",
-			nodefined : " ei ole m&auml;&auml;ritelty!",
-			novalue : " paluuarvo vaaditaan!",
-			customarray : "Custom function should return array!",
-			customfcheck : "Custom function should be present in case of custom checking!"
+			nodefined: " ei ole m&auml;&auml;ritelty!",
+			novalue: " paluuarvo vaaditaan!",
+			customarray: "Custom function should return array!",
+			customfcheck: "Custom function should be present in case of custom checking!"
 		}
 	},
-	view : {
-	    caption: "N&auml;  rivi",
+	view: {
+	    caption: "N&auml;yt&auml; rivi",
 	    bClose: "Sulje"
 	},
-	del : {
+	del: {
 	    caption: "Poista",
-	    msg: "Poista valitut  rivi(t)?",
+	    msg: "Poista valitut rivit?",
 	    bSubmit: "Poista",
 	    bCancel: "Peru"
 	},
-	nav : {
+	nav: {
 		edittext: " ",
 	    edittitle: "Muokkaa valittu rivi",
-		addtext:" ",
+		addtext: " ",
 	    addtitle: "Uusi rivi",
 	    deltext: " ",
 	    deltitle: "Poista valittu rivi",
@@ -74,37 +72,37 @@ $.jgrid = {
 	    alertcap: "Varoitus",
 	    alerttext: "Valitse rivi",
 		viewtext: "",
-		viewtitle: "Nayta valitut rivit"
+		viewtitle: "N&auml;yta valitut rivit"
 	},
-	col : {
-	    caption: "Nayta/Piilota sarakkeet",
+	col: {
+	    caption: "N&auml;yta/Piilota sarakkeet",
 	    bSubmit: "OK",
 	    bCancel: "Peru"	
 	},
 	errors : {
-		errcap : "Virhe",
-		nourl : "url asettamatta",
+		errcap: "Virhe",
+		nourl: "url asettamatta",
 		norecords: "Ei muokattavia tietoja",
-	    model : "Pituus colNames <> colModel!"
+	    model: "Pituus colNames <> colModel!"
 	},
-	formatter : {
-		integer : {thousandsSeparator: "", defaultValue: '0'},
-		number : {decimalSeparator:",", thousandsSeparator: "", decimalPlaces: 2, defaultValue: '0,00'},
-		currency : {decimalSeparator:",", thousandsSeparator: "", decimalPlaces: 2, prefix: "", suffix:"", defaultValue: '0,00'},
-		date : {
+	formatter: {
+		integer: {thousandsSeparator: "", defaultValue: '0'},
+		number: {decimalSeparator:",", thousandsSeparator: "", decimalPlaces: 2, defaultValue: '0,00'},
+		currency: {decimalSeparator:",", thousandsSeparator: "", decimalPlaces: 2, prefix: "", suffix:"", defaultValue: '0,00'},
+		date: {
 			dayNames:   [
 				"Su", "Ma", "Ti", "Ke", "To", "Pe", "La",
-				"Sunnuntai", "Maanantai", "Tiista", "Keskiviikko", "Torstai", "Perjantai", "Lauantai"
+				"Sunnuntai", "Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai"
 			],
 			monthNames: [
 				"Tam", "Hel", "Maa", "Huh", "Tou", "Kes", "Hei", "Elo", "Syy", "Lok", "Mar", "Jou",
 				"Tammikuu", "Helmikuu", "Maaliskuu", "Huhtikuu", "Toukokuu", "Kes&auml;kuu", "Hein&auml;kuu", "Elokuu", "Syyskuu", "Lokakuu", "Marraskuu", "Joulukuu"
 			],
-			AmPm : ["am","pm","AM","PM"],
+			AmPm: ["am","pm","AM","PM"],
 			S: function (j) {return j < 11 || j > 13 ? ['st', 'nd', 'rd', 'th'][Math.min((j - 1) % 10, 3)] : 'th'},
 			srcformat: 'Y-m-d',
 			newformat: 'd/m/Y',
-			masks : {
+			masks: {
 	            ISO8601Long:"Y-m-d H:i:s",
 	            ISO8601Short:"Y-m-d",
 	            ShortDate: "d.m.Y",
@@ -122,8 +120,8 @@ $.jgrid = {
 		baseLinkUrl: '',
 		showAction: '',
 	    target: '',
-	    checkbox : {disabled:true},
-		idName : 'id'
+	    checkbox: {disabled:true},
+		idName: 'id'
 	}
 };
 // FI
