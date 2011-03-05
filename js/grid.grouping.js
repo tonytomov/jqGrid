4878a236916daf873e44aa508ac3c591ee64519a
Tony Tomov <tony@trirand.com>
2011-03-05 12:58:52 +0200

    Fix expand/collapse icon in grouping

--- js/grid.grouping.js
+++ js/grid.grouping.js
@@ -91,7 +91,7 @@ $.jgrid.extend({
 			plus = grp.plusicon,
 			tar = $("#"+hid),
 			r = tar[0].nextSibling,
-			tarspan = $("#"+hid+" span"),
+			tarspan = $("#"+hid+" span."+"tree-wrap-"+$t.p.direction),
 			collapsed = false;
 			if( tarspan.hasClass(minus) ) {
 				if(grp.showSummaryOnHide && grp.groupSummary[0]) {
