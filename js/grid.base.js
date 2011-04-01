a6fdb39bd86f3c4d75554c834ad0ad4a51dfbc99
Tony Tomov <tony@trirand.com>
2011-04-01 12:32:04 +0300

    Moving the sr=null to the right place

--- js/grid.base.js
+++ js/grid.base.js
@@ -2514,11 +2514,11 @@ $.jgrid.extend({
 			if(typeof(rowid) !== "undefined" ) {
 				sr = rowid === t.p.selrow ? t.p.selrow : rowid;
 				$("#"+$.jgrid.jqID(t.p.id)+" tbody:first tr#"+$.jgrid.jqID(sr)).removeClass("ui-state-highlight").attr("aria-selected","false");
-				sr = null;
 				if(t.p.multiselect) {
 					$("#jqg_"+$.jgrid.jqID(t.p.id)+"_"+$.jgrid.jqID(sr)).attr("checked",false);
 					$("#cb_"+$.jgrid.jqID(t.p.id)).attr("checked",false);
 				}
+				sr = null;
 			} else if(!t.p.multiselect) {
 				if(t.p.selrow) {
 					$("#"+$.jgrid.jqID(t.p.id)+" tbody:first tr#"+$.jgrid.jqID(t.p.selrow)).removeClass("ui-state-highlight").attr("aria-selected","false");
