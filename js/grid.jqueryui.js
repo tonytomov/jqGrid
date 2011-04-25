a744c203f0e252a67739120b7e4fcd6aaf034ee6
Tony Tomov <tony@trirand.com>
2011-04-25 19:40:02 +0300

    Fix for sortable columns according to the unique id in column header

--- js/grid.jqueryui.js
+++ js/grid.jqueryui.js
@@ -52,13 +52,13 @@ $.jgrid.extend({
 	sortableColumns : function (tblrow)
 	{
 		return this.each(function (){
-			var ts = this;
+			var ts = this, tid= $t.p.id;
 			function start() {ts.p.disableClick = true;}
 			var sortable_opts = {
 				"tolerance" : "pointer",
 				"axis" : "x",
 				"scrollSensitivity": "1",
-				"items": '>th:not(:has(#jqgh_cb,#jqgh_rn,#jqgh_subgrid),:hidden)',
+				"items": ">th:not(:has(#jqgh_"+tid+"_cb"+",#jqgh_"+tid+"_rn"+",#jqgh_"+tid+"_subgrid),:hidden)",
 				"placeholder": {
 					element: function(item) {
 						var el = $(document.createElement(item[0].nodeName))
@@ -72,14 +72,14 @@ $.jgrid.extend({
 					}
 				},
 				"update": function(event, ui) {
-					var p = $(ui.item).parent();
-					var th = $(">th", p);
-					var colModel = ts.p.colModel;
-					var cmMap = {};
+					var p = $(ui.item).parent(),
+					th = $(">th", p),
+					colModel = ts.p.colModel,
+					cmMap = {}, tid= this.p.id+"_";
 					$.each(colModel, function(i) { cmMap[this.name]=i; });
 					var permutation = [];
 					th.each(function(i) {
-						var id = $(">div", this).get(0).id.replace(/^jqgh_/, "");
+						var id = $(">div", this).get(0).id.replace(/^jqgh_/, "").replace(tid,"");
 							if (id in cmMap) {
 								permutation.push(cmMap[id]);
 							}
