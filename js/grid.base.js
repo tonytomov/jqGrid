29c6d85fb1648ba2f66edba27dcc7054a27c07ad
Tony Tomov <tony@trirand.com>
2011-06-20 16:38:56 +0800

    Fix clearing the index local data correct way.

--- js/grid.base.js
+++ js/grid.base.js
@@ -3156,7 +3156,7 @@ $.jgrid.extend({
 			if($t.p.footerrow && clearfooter) { $(".ui-jqgrid-ftable td",$t.grid.sDiv).html("&#160;"); }
 			$t.p.selrow = null; $t.p.selarrrow= []; $t.p.savedRow = [];
 			$t.p.records = 0;$t.p.page=1;$t.p.lastpage=0;$t.p.reccount=0;
-			$t.p.data = []; $t.p_index = {};
+			$t.p.data = []; $t.p._index = {};
 			$t.updatepager(true,false);
 		});
 	},
