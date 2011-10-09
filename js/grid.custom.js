854f28a7c8e04321cfffd7ec1ac80bd49a6c716e
Tony Tomov <tony@trirand.com>
2011-10-09 12:29:20 +0300

    Optimizations in setGroupHeader

--- js/grid.custom.js
+++ js/grid.custom.js
@@ -538,6 +538,7 @@ $.jgrid.extend({
 			groupHeaders: []
 		},o  || {});
 		return this.each(function(){
+			this.p.groupHeader = o;
 			var ts = this,
 			i, cmi, skip = 0, $tr, $colHeader, th, $th, thStyle,
 			iCol,
@@ -549,57 +550,43 @@ $.jgrid.extend({
 			colModel = ts.p.colModel,
 			cml = colModel.length,
 			ths = ts.grid.headers,
-			$gview = $(ts).closest("div.ui-jqgrid-view"),
-			$gbox = $gview.parent(),
-			$bdiv = $gview.children("div.ui-jqgrid-bdiv"),
-			$hdiv = $gview.children("div.ui-jqgrid-hdiv"),
-			$htable = $hdiv.children("div.ui-jqgrid-hbox").children("table.ui-jqgrid-htable"),
-			$trLabels = $htable.children("thead").children("tr.ui-jqgrid-labels:last"),
+			$htable = $("table.ui-jqgrid-htable", ts.grid.hDiv),
+			$trLabels = $htable.children("thead").children("tr.ui-jqgrid-labels:last").addClass("jqg-second-row-header"),
 			$thead = $htable.children("thead"),
 			$theadInTable,
 			originalResizeStop,
-			$firstHeaderRow = $('<tr>', {role: "row", "aria-hidden": "true"}).addClass("jqg-first-row-header").css("height", "auto"),
-			$firstRow,
-			denySelectionOnDoubleClick = function ($el) {
-				// see http://stackoverflow.com/questions/2132172/disable-text-highlighting-on-double-click-in-jquery/2132230#2132230
-				if ($.browser.mozilla) {//Firefox
-					$el.css('MozUserSelect', 'none');
-				} else if ($.browser.msie) {//IE
-					$el.bind('selectstart', function () {
-						return false;
-					});
-				} else {//Opera, etc.
-					$el.mousedown(function () {
-						return false;
-					});
-				}
-			},
+			$firstHeaderRow = $htable.find(".jqg-first-row-header");
+			if($firstHeaderRow.html() === null) {
+				$firstHeaderRow = $('<tr>', {role: "row", "aria-hidden": "true"}).addClass("jqg-first-row-header").css("height", "auto");
+			} else {
+				$firstHeaderRow.empty();
+			}
+			var $firstRow,
 			inColumnHeader = function (text, columnHeaders) {
-				var i = 0, length = columnHeaders.length;
-				for (; i < length; i++) {
-					if (columnHeaders[i].startColumnName === text) {
-						return i;
-					}
-				}
-				return -1;
+                    var i = 0, length = columnHeaders.length;
+                    for (; i < length; i++) {
+                        if (columnHeaders[i].startColumnName === text) {
+                            return i;
+                        }
+                    }
+        
+                    return -1;
 			};
 
 			$(ts).prepend($thead);
-			$tr = $('<tr>', {role: "rowheader"}).addClass("ui-jqgrid-labels");
+			$tr = $('<tr>', {role: "rowheader"}).addClass("ui-jqgrid-labels jqg-third-row-header");
 			for (i = 0; i < cml; i++) {
 				th = ths[i].el;
 				$th = $(th);
-				denySelectionOnDoubleClick($th); // needed for Firefox to prevent selection on doubleclick
 				cmi = colModel[i];
 				// build the next cell for the first header row
-				thStyle = {height: '0px', width: ths[i].width + 'px', display: (cmi.hidden ? 'none' : '')};
-				$("<th>", {role: 'gridcell'}).css(thStyle).appendTo($firstHeaderRow);
+				thStyle = { height: '0px', width: ths[i].width + 'px', display: (cmi.hidden ? 'none' : '')};
+				$("<th>", {role: 'gridcell'}).css(thStyle).addClass("ui-first-th-ltr").appendTo($firstHeaderRow);
 
 				th.style.width = ""; // remove unneeded style
 				iCol = inColumnHeader(cmi.name, o.groupHeaders);
 				if (iCol >= 0) {
 					cghi = o.groupHeaders[iCol];
-					//startColumnName = cghi.startColumnName;
 					numberOfColumns = cghi.numberOfColumns;
 					titleText = cghi.titleText;
 
@@ -615,11 +602,15 @@ $.jgrid.extend({
 					// The text will be over the cVisibleColumns columns
 					$colHeader = $('<th>', {colspan: String(cVisibleColumns), role: "columnheader"})
 						.addClass("ui-state-default ui-th-column-header ui-th-ltr")
+						.height(22)
 						.html(titleText);
 					if (ts.p.headertitles) {
 						$colHeader.attr("title", $colHeader.text());
 					}
-					denySelectionOnDoubleClick($colHeader);
+					// hide if not a visible cols
+					if( cVisibleColumns === 0) {
+						$colHeader.hide();
+					}
 
 					$th.before($colHeader); // insert new column header before the current
 					$tr.append(th);         // move the current header in the next row
@@ -633,14 +624,14 @@ $.jgrid.extend({
 							$th.attr("rowspan", "2");
 						} else {
 							$('<th>', {role: "columnheader"})
-								.addClass("ui-state-default ui-th-column-header")
-								.css("display", cmi.hidden ? 'none' : '')
+								.addClass("ui-state-default ui-th-column-header ui-th-ltr")
+								.css({"display": cmi.hidden ? 'none' : ''})
 								.insertBefore($th);
 							$tr.append(th);
 						}
 					} else {
 						// move the header to the next row
-						$th.css({"padding-top": "2px", height: "19px"});
+						//$th.css({"padding-top": "2px", height: "19px"});
 						$tr.append(th);
 						skip--;
 					}
@@ -677,25 +668,12 @@ $.jgrid.extend({
 			}
 			$firstRow = $theadInTable.find("tr.jqg-first-row-header");
 			ts.p.resizeStop = function (nw, idx) {
-				var newWidth = this.newWidth;
-				/*
-				$firstRow.find("th:nth-child(" + (idx + 1) + ")").width(nw);
-
-				// ajust the size of gview, gbox and the pager base on the width of htable
-				$gview.width(newWidth);
-				$gbox.width(newWidth);
-				$bdiv.width(newWidth);
-				$hdiv.width(newWidth);
-				$gbox.children("div.ui-jqgrid-pager").width(newWidth);     // set pager width
-				$gview.children("div.ui-jqgrid-toppager").width(newWidth); // set toppager width
-				$gview.children("div.ui-jqgrid-sdiv").width(newWidth);     // set footer width
-				*/
-
+				$firstRow.find('th').eq(idx).width(nw);
 				if ($.isFunction(originalResizeStop)) {
 					originalResizeStop.call(ts, nw, idx);
 				}
 			};
-		});
+		});				
 	}
 });
 })(jQuery);
