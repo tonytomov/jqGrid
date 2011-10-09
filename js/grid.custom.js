6e4a9e79c7e1663cf1bb76f6e3b6b47c44256590
Tony Tomov <tony@trirand.com>
2011-10-09 19:07:28 +0800

    We must use setGridParam when change groupHeader

--- js/grid.custom.js
+++ js/grid.custom.js
@@ -534,7 +534,7 @@ $.jgrid.extend({
 						$("tr.jqg-second-row-header", $t.grid.hDiv).remove();
 					}
 					// else find better way when colSpanStyle is true
-					gh = null;
+					$($t).jqGrid('setGridParam',{ 'groupHeader': null});
 				}
 			}
 		});
