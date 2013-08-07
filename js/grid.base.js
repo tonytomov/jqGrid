f9500740551ce2e9b9f5c953b896f3242fab1e84
Tony Tomov <tony@trirand.com>
2013-08-07 17:39:54 +0300

    Revert back the fix when resize grid and then use show/hide button. The fix is wrong and causes many more problems

--- js/grid.base.js
+++ js/grid.base.js
@@ -2799,7 +2799,7 @@ $.fn.jqGrid = function( pin ) {
 		}
 		hb = null;
 		if(ts.p.caption) {
-			var tdt = ts.p.datatype, gheight;
+			var tdt = ts.p.datatype;
 			if(ts.p.hidegrid===true) {
 				$(".ui-jqgrid-titlebar-close",grid.cDiv).click( function(e){
 					var onHdCl = $.isFunction(ts.p.onHeaderClick),
@@ -2814,7 +2814,6 @@ $.fn.jqGrid = function( pin ) {
 					counter = $(elems,"#gview_"+$.jgrid.jqID(ts.p.id)).length;
 
 					if(ts.p.gridstate === 'visible') {
-						gheight = $("#gbox_"+$.jgrid.jqID(ts.p.id)).height();
 						$(elems,"#gbox_"+$.jgrid.jqID(ts.p.id)).slideUp("fast", function() {
 							counter--;
 							if (counter === 0) {
@@ -2823,7 +2822,6 @@ $.fn.jqGrid = function( pin ) {
 								if($("#gbox_"+$.jgrid.jqID(ts.p.id)).hasClass("ui-resizable")) { $(".ui-resizable-handle","#gbox_"+$.jgrid.jqID(ts.p.id)).hide(); }
 								$(ts).triggerHandler("jqGridHeaderClick", [ts.p.gridstate,e]);
 								if(onHdCl) {if(!hg) {ts.p.onHeaderClick.call(ts,ts.p.gridstate,e);}}
-								$("#gbox_"+$.jgrid.jqID(ts.p.id)).height( $("#gview_"+$.jgrid.jqID(ts.p.id)).height());
 							}
 						});
 					} else if(ts.p.gridstate === 'hidden'){
@@ -2838,7 +2836,6 @@ $.fn.jqGrid = function( pin ) {
 								if(onHdCl) {if(!hg) {ts.p.onHeaderClick.call(ts,ts.p.gridstate,e);}}
 							}
 						});
-						$("#gbox_"+$.jgrid.jqID(ts.p.id)).height(gheight);
 					}
 					return false;
 				});
