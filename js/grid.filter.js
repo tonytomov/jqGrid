ca6b9b1aec7373346148f4b6704e236304d57ba8
Dr. Oleg Kiriljuk <oleg.kiriljuk@ok-soft-gmbh.com>
2013-12-02 18:57:59 +0100

    fix applying searching without saving the data in rule
    It's not really important whether the control have jQuery UI Autocomplete or not. It's important just that **at the moment when the user click "Search" button the current data from the input fields should be used**. One need just trigger `change` event on the input which have the focus to be sure that `rule` will contain the data which the user see in the Search dialog. It's important that the `change` event will be immediately processed and the `rule` be updated. The state of jQuery UI Autocomplete at the moment is not important.

--- js/grid.filter.js
+++ js/grid.filter.js
@@ -391,13 +391,10 @@ $.fn.jqFilter = function( arg ) {
 				$(".data",trpar).empty().append( elm );
 				$.jgrid.bindEv.call($t, elm, cm.searchoptions);
 				$(".input-elm",trpar).bind('change',function( e ) {
-					var tmo = $(this).hasClass("ui-autocomplete-input") ? 200 :0;
-					setTimeout(function(){
-						var elem = e.target;
-						rule.data = elem.nodeName.toUpperCase() === "SPAN" && cm.searchoptions && $.isFunction(cm.searchoptions.custom_value) ?
-							cm.searchoptions.custom_value.call($t, $(elem).children(".customelement:first"), 'get') : elem.value;
-						that.onchange(); // signals that the filter has changed
-					}, tmo);
+					var elem = e.target;
+					rule.data = elem.nodeName.toUpperCase() === "SPAN" && cm.searchoptions && $.isFunction(cm.searchoptions.custom_value) ?
+						cm.searchoptions.custom_value.call($t, $(elem).children(".customelement:first"), 'get') : elem.value;
+					that.onchange(); // signals that the filter has changed
 				});
 				setTimeout(function(){ //IE, Opera, Chrome
 				rule.data = $(elm).val();
