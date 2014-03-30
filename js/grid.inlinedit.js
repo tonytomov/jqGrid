86ade36336482684dfb17c293cf4986f317f6df3
Tony Tomov <tony@trirand.com>
2014-03-30 08:02:46 +0300

    Fix focus on disabled input in inline edit.

--- js/grid.inlinedit.js
+++ js/grid.inlinedit.js
@@ -96,7 +96,12 @@ $.jgrid.extend({
 				if(cnt > 0) {
 					svr.id = rowid; $t.p.savedRow.push(svr);
 					$(ind).attr("editable","1");
-					setTimeout(function(){$("td:eq("+focus+") input",ind).focus();},0);
+					setTimeout(function(){ 
+						var fe = $("td:eq("+focus+") input",ind).not(":disabled"); 
+						if(fe.length > 0) {
+							fe.focus();
+						}
+					},0);
 					if(o.keys===true) {
 						$(ind).bind("keydown",function(e) {
 							if (e.keyCode === 27) {
