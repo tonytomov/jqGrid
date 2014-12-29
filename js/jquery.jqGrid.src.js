6417ac55b15bfe8c6b519f8394f7ff0fa462cdeb
Dr. Oleg Kiriljuk <oleg.kiriljuk@ok-soft-gmbh.com>
2014-12-29 15:17:16 +0100

    add setColWidth method to base module
    Add setColWidth method published before on
    https://github.com/OlegKi/jqGrid-plugins

--- js/jquery.jqGrid.src.js
+++ js/jquery.jqGrid.src.js
@@ -4020,6 +4020,36 @@ $.jgrid.extend({
 					break;
 			}
 		});
+	},
+	setColWidth: function (iCol, newWidth, adjustGridWidth) {
+		return this.each(function () {
+			var $self = $(this), grid = this.grid, colName, colModel, i, nCol;
+			if (typeof iCol === "string") {
+				// the first parametrer is column name instead of index
+				colName = iCol;
+				colModel = $self.jqGrid("getGridParam", "colModel");
+				for (i = 0, nCol = colModel.length; i < nCol; i++) {
+					if (colModel[i].name === colName) {
+						iCol = i;
+						break;
+					}
+				}
+				if (i >= nCol) {
+					return; // error: non-existing column name specified as the first parameter
+				}
+			} else if (typeof iCol !== "number") {
+				return; // error: wrong parameters
+			}
+			grid.resizing = { idx: iCol };
+			grid.headers[iCol].newWidth = newWidth;
+			if (adjustGridWidth !== false) {
+				grid.newWidth = grid.width + newWidth - grid.headers[iCol].width;
+			}
+			grid.dragEnd();   // adjust column width
+			if (adjustGridWidth !== false) {
+				$self.jqGrid("setGridWidth", grid.newWidth, false); // adjust grid width too
+			}
+		});
 	}
 });
 })(jQuery);
