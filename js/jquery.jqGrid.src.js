858628cf8f448821631786ab74b6e7010b913e77
Dr. Oleg Kiriljuk <oleg.kiriljuk@ok-soft-gmbh.com>
2014-12-29 14:40:11 +0100

    update jquery.jqGrid.src.js, jquery.jqGrid.min.js, jquery.jqGrid.min.map

--- js/jquery.jqGrid.src.js
+++ js/jquery.jqGrid.src.js
@@ -2320,7 +2320,7 @@ $.fn.jqGrid = function( pin ) {
 					if(ts.p.lastsort === idxcol && ts.p.sortorder === sor && !reload) { return; }
 					ts.p.sortorder = sor;
 				}
-				var previousSelectedTh = ts.grid.headers[ts.p.lastsort].el, newSelectedTh = ts.p.frozenColumns ?  obj : ts.grid.headers[idxcol].el;
+				var previousSelectedTh = ts.grid.headers[ts.p.lastsort] ? ts.grid.headers[ts.p.lastsort].el : null, newSelectedTh = ts.p.frozenColumns ?  obj : ts.grid.headers[idxcol].el;
 
 				$("span.ui-grid-ico-sort",previousSelectedTh).addClass('ui-state-disabled');
 				$(previousSelectedTh).attr("aria-selected","false");
@@ -10806,7 +10806,13 @@ $.jgrid.extend({
 					}
 				};
 				$("tbody:first",$t).sortable(opts);
-				$("tbody:first",$t).disableSelection();
+				if ($.isFunction($.fn.disableSelection)) {
+					// disableSelection exist starting with jQuery UI 1.6,
+					// but it's declared as deprecated since jQuery UI 1.9
+					// see http://jqueryui.com/upgrade-guide/1.9/#deprecated-disableselection-and-enableselection
+					// so we use disableSelection only if it exists
+					$("tbody:first>.jqgrow",$t).disableSelection();
+				}
 			}
 		});
 	},
