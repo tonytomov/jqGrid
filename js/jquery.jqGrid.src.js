0c6bcfd9af6c175e7387078380b6f54d96f6af3c
Dr. Oleg Kiriljuk <oleg.kiriljuk@ok-soft-gmbh.com>
2015-01-15 15:14:27 +0100

    fixing the bug with grouping

--- js/jquery.jqGrid.src.js
+++ js/jquery.jqGrid.src.js
@@ -1788,7 +1788,7 @@ $.fn.jqGrid = function( pin ) {
 						if(!p.groupingView._locgr) {
 							groupingPrepare.call($self, rd, j );
 						}
-						clearArray(rowData); //rowData = [];
+						rowData = [];
 					}
 					if(locdata || p.treeGrid === true) {
 						rd[xmlid] = stripPref(p.idPrefix, rid);
@@ -1991,7 +1991,7 @@ $.fn.jqGrid = function( pin ) {
 					if(!p.groupingView._locgr) {
 						groupingPrepare.call($self, rd, i);
 					}
-					clearArray(rowData); // rowData = [];
+					rowData = [];
 				}
 				if(locdata || p.treeGrid===true) {
 					rd[locid] = stripPref(p.idPrefix, idr);
