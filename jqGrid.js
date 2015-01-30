f1fd0267cdece12d73249d6e7dc7ed6de1d1bd70
Tony Tomov <tony@trirand.com>
2015-01-30 17:02:08 +0200

    add jqGrid file to build the module for requierejs

---
+++ jqGrid.js
@@ -0,0 +1,26 @@
+require([
+	"jquery",
+
+	// grid
+	"jqgrid/grid.base",
+	"jqgrid/grid.celledit",
+	"jqgrid/grid.common",
+	"jqgrid/grid.filter",
+	"jqgrid/grid.formedit",
+	"jqgrid/grid.grouping",
+	"jqgrid/grid.import",
+	"jqgrid/grid.inlinedit",
+	"jqgrid/grid.jqueryui",
+	"jqgrid/grid.pivot",
+	"jqgrid/grid.subgrid",
+	"jqgrid/grid.treegrid",
+	"jqgrid/jqDnR",
+	"jqgrid/jqModal",
+	"jqgrid/jquery.fmatter",
+	"jqgrid/JsonXml",
+	"jqgrid/addons/ui.multiselect"
+
+], function ($) {
+	"use strict";
+	console.log("jqGrid loaded!");
+});
