e9df8d6aea5620cfa192f80239e376737eedc162
Tony Tomov <tony@trirand.com>
2015-01-30 17:04:16 +0200

    Adding config.js needed for requierejs

---
+++ config.js
@@ -0,0 +1,17 @@
+require.config({
+	"baseUrl": "../",
+
+	"paths": {
+
+		// jqgrid
+		"jqgrid": "js",
+
+		// plugins
+		"css": "bower_components/require-css/css.min",
+
+		// jquery
+		"jquery": "bower_components/jquery/dist/jquery.min",
+		"jquery-ui": "bower_components/jquery-ui/ui"
+
+	}
+});
