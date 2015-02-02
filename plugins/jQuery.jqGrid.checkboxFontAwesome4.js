b5f7b64fd9c5b02f182288c89230945efe9b9af1
Dr. Oleg Kiriljuk <oleg.kiriljuk@ok-soft-gmbh.com>
2015-02-02 02:07:15 +0100

    use tabs instead of spaces

--- plugins/jQuery.jqGrid.checkboxFontAwesome4.js
+++ plugins/jQuery.jqGrid.checkboxFontAwesome4.js
@@ -8,19 +8,19 @@
  */
 /*global jQuery */
 (function ($) {
-    "use strict";
-    /*jslint unparam: true */
-    $.extend($.fn.fmatter, {
-        checkboxFontAwesome4: function (cellValue, options) {
-            var title = options.colModel.title !== false ? ' title="' + (options.colName || options.colModel.label || options.colModel.name) + '"' : '',
+	"use strict";
+	/*jslint unparam: true */
+	$.extend($.fn.fmatter, {
+		checkboxFontAwesome4: function (cellValue, options) {
+			var title = options.colModel.title !== false ? ' title="' + (options.colName || options.colModel.label || options.colModel.name) + '"' : '',
 				strCellValue = String(cellValue).toLowerCase(),
 				editoptions = options.colModel.editoptions,
 				editYes = editoptions != null && typeof editoptions.value === "string" ? editoptions.value.split(":")[0] : "yes";
-            return (cellValue === 1 || strCellValue === "1" || cellValue === true || strCellValue === "true" || strCellValue === "yes" || strCellValue === editYes) ?
-                '<i class="fa fa-check-square-o fa-lg"' + title + '></i>' :
-                '<i class="fa fa-square-o fa-lg"' + title + '></i>';
-        }
-    });
+			return (cellValue === 1 || strCellValue === "1" || cellValue === true || strCellValue === "true" || strCellValue === "yes" || strCellValue === editYes) ?
+				'<i class="fa fa-check-square-o fa-lg"' + title + '></i>' :
+				'<i class="fa fa-square-o fa-lg"' + title + '></i>';
+		}
+	});
 	$.extend(true,$.jgrid, {
 		cmTemplate: {
 			booleanCheckboxFa: {
@@ -42,12 +42,12 @@
 			},
 		}
 	});
-    $.extend($.fn.fmatter.checkboxFontAwesome4, {
-        unformat: function (cellValue, options, elem) {
-            var cbv = (options.colModel.editoptions != null && options.colModel.editoptions.value) ?
+	$.extend($.fn.fmatter.checkboxFontAwesome4, {
+		unformat: function (cellValue, options, elem) {
+			var cbv = (options.colModel.editoptions != null && options.colModel.editoptions.value) ?
 					options.colModel.editoptions.value.split(":") :
 					["Yes", "No"];
-            return $(">i", elem).hasClass("fa-check-square-o") ? cbv[0] : cbv[1];
-        }
-    });
+			return $(">i", elem).hasClass("fa-check-square-o") ? cbv[0] : cbv[1];
+		}
+	});
 }(jQuery));
