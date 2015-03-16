fa72061e67778405c43b86c30c08ceaa947689fe
Tony Tomov <tony@trirand.com>
2015-03-16 16:18:34 +0200

    Fix loading the default lang according to the new lang changes

--- js/grid.base.js
+++ js/grid.base.js
@@ -904,6 +904,9 @@ $.fn.jqGrid = function( pin ) {
 			localData = pin.data;
 			pin.data = [];
 		}
+		if(!pin.hasOwnProperty("regional")) {
+			pin.regional = 'en';
+		}
 
 		var p = $.extend(true,{
 			url: "",
@@ -1016,7 +1019,7 @@ $.fn.jqGrid = function( pin ) {
 			scrollLeftOffset : "100%", //percent
 			storeNavOptions: false,
 			regional :  "en"
-		}, $.jgrid.defaults || {}, pin );
+		}, $.jgrid.regional[pin.regional].defaults || {}, pin );
 		if (localData !== undefined) {
 			p.data = localData;
 			pin.data = localData;
