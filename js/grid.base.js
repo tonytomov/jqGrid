f9022930e3b0fe02d0ce12315c46b40f03de07a8
Tony Tomov <tony@trirand.com>
2015-08-08 15:32:08 +0300

    pager now can use a table-condensed. This way jqGrid support a table condensed

--- js/grid.base.js
+++ js/grid.base.js
@@ -935,6 +935,7 @@ $.extend($.jgrid,{
 				scrollBox : "ui-widget-content",
 				multiBox : "cbox",
 				pagerBox : "ui-state-default ui-corner-bottom",
+				pagerTable : "",
 				toppagerBox : "ui-state-default",
 				pgInput : "ui-corner-all",
 				pgSelectBox : "ui-widget-content ui-corner-all",
@@ -1049,6 +1050,7 @@ $.extend($.jgrid,{
 				scrollBox : "",
 				multiBox : "checkbox",
 				pagerBox : "",
+				pagerTable : "table",
 				toppagerBox : "",
 				pgInput : "form-control",
 				pgSelectBox : "form-control",
@@ -2640,7 +2642,7 @@ $.fn.jqGrid = function( pin ) {
 			pgcnt = "pg_"+pgid;
 			lft = pgid+"_left"; cent = pgid+"_center"; rgt = pgid+"_right";
 			$("#"+$.jgrid.jqID(pgid) )
-			.append("<div id='"+pgcnt+"' class='ui-pager-control' role='group'><table class='ui-pg-table ui-common-table ui-pager-table'><tbody><tr><td id='"+lft+"' align='left'></td><td id='"+cent+"' align='center' style='white-space:pre;'></td><td id='"+rgt+"' align='right'></td></tr></tbody></table></div>")
+			.append("<div id='"+pgcnt+"' class='ui-pager-control' role='group'><table " + getstyle(stylemodule, 'pagerTable', false, 'ui-pg-table ui-common-table ui-pager-table') + "><tbody><tr><td id='"+lft+"' align='left'></td><td id='"+cent+"' align='center' style='white-space:pre;'></td><td id='"+rgt+"' align='right'></td></tr></tbody></table></div>")
 			.attr("dir","ltr"); //explicit setting
 			if(ts.p.rowList.length >0){
 				str = "<td dir=\""+dir+"\">";
