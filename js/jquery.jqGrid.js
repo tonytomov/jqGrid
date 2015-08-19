642612365e9109d2e4ccc4a8ac495e476f63745b
Tony Tomov <tony@trirand.com>
2015-08-19 13:59:21 +0300

    Update the build

--- js/jquery.jqGrid.js
+++ js/jquery.jqGrid.js
@@ -1,6 +1,6 @@
 /**
 *
-* @license Guriddo jqGrid JS - v5.0.0 - 2015-08-18
+* @license Guriddo jqGrid JS - v5.0.0 - 2015-08-19
 * Copyright(c) 2008, Tony Tomov, tony@trirand.com
 * 
 * License: http://guriddo.net/?page_id=103334
@@ -5647,8 +5647,9 @@ $.extend($.jgrid,{
 		if(p.resize === undefined) {p.resize=true;}
 		if (p.drag) {
 			$(mh).css('cursor','move');
-			if($.fn.jqDrag) {
-				$(mw).jqDrag(mh);
+			if($.fn.tinyDraggable) {
+				//$(mw).jqDrag(mh);
+				$(mw).tinyDraggable({ handle:"#"+$.jgrid.jqID(mh.id) });
 			} else {
 				try {
 					$(mw).draggable({handle: $("#"+$.jgrid.jqID(mh.id))});
@@ -13835,6 +13836,28 @@ i=function(e,h,k,aR){
 },
 f=function(k){return parseInt(E.css(k),10)||false;},
 f1=function(k){return parseInt(E1.css(k),10)||false;};
+/*
+	jQuery tinyDraggable v1.0.2
+    Copyright (c) 2014 Simon Steinberger / Pixabay
+    GitHub: https://github.com/Pixabay/jQuery-tinyDraggable
+    More info: https://pixabay.com/blog/posts/p-52/
+	License: http://www.opensource.org/licenses/mit-license.php
+*/
+$.fn.tinyDraggable = function(options){
+	var settings = $.extend({ handle: 0, exclude: 0 }, options);
+	return this.each(function(){
+	    var dx, dy, el = $(this), handle = settings.handle ? $(settings.handle, el) : el;
+        handle.on({
+        mousedown: function(e){
+			if (settings.exclude && ~$.inArray(e.target, $(settings.exclude, el))) { return; }
+			e.preventDefault();
+			var os = el.offset(); dx = e.pageX-os.left, dy = e.pageY-os.top;
+			$(document).on('mousemove.drag', function(e){ el.offset({top: e.pageY-dy, left: e.pageX-dx}); });
+			},
+			mouseup: function(e){ $(document).off('mousemove.drag'); }
+		});
+	});
+};
 
 //module begin
 $.fn.jqm=function(o){
