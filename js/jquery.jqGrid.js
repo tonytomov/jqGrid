12ad027a7a31e9035d3c679c8b6adeb37e3fa701
Tony Tomov <tony@trirand.com>
2015-09-28 16:48:25 +0300

    Update build using uglify

--- js/jquery.jqGrid.js
+++ js/jquery.jqGrid.js
@@ -1,6 +1,6 @@
 /**
 *
-* @license Guriddo jqGrid JS - v5.0.0 - 2015-09-27
+* @license Guriddo jqGrid JS - v5.0.0 - 2015-09-28
 * Copyright(c) 2008, Tony Tomov, tony@trirand.com
 * 
 * License: http://guriddo.net/?page_id=103334
