9454e5fe4315391a956e82aa4569056cf503ffc2
Tony Tomov <tony@trirand.com>
2016-02-04 13:00:43 +0200

    Changes in italian language file

--- js/i18n/grid.locale-it.js
+++ js/i18n/grid.locale-it.js
@@ -116,85 +116,54 @@ $.jgrid.regional["it"] = {
 		norecords: "Non ci sono record da elaborare",
 		model : "Lunghezza dei colNames <> colModel!"
 	},
-	formatter : {
-		integer : {thousandsSeparator: ",", defaultValue: '0'},
-		number : {decimalSeparator:".", thousandsSeparator: ",", decimalPlaces: 2, defaultValue: '0.00'},
-		currency : {decimalSeparator:".", thousandsSeparator: ",", decimalPlaces: 2, prefix: "", suffix:"", defaultValue: '0.00'},
-		date : {
-			dayNames:   [
-				"Dom", "Lun", "Mar", "Mer", "Gio", "Ven", "Sab",
-				"Domenica", "Lunedi", "Martedi", "Mercoledi", "Giovedi", "Venerdi", "Sabato"
-			],
-			monthNames: [
-				"Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic",
-				"Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"
-			],
-			AmPm : ["am","pm","AM","PM"],
-			S: function (j) {return j < 11 || j > 13 ? ['st', 'nd', 'rd', 'th'][Math.min((j - 1) % 10, 3)] : 'th';},
-			srcformat: 'Y-m-d',
-			newformat: 'n/j/Y',
+	formatter:{
+		integer:{ 
+			thousandsSeparator:".",
+			defaultValue:"0"
+		},
+		number:{
+			decimalSeparator:",",
+			thousandsSeparator:".",
+			decimalPlaces:2,
+			defaultValue:"0,00"
+		},
+		currency:{
+			decimalSeparator:",",
+			thousandsSeparator:".",
+			decimalPlaces:2,
+			prefix:"",
+			suffix:"",
+			defaultValue:"0,00"
+		},
+		date:{
+			dayNames:["Dom","Lun","Mar","Mer","Gio","Ven","Sab","Domenica","Lunedì","Martedì","Mercoledì","Giovedì","Venerdì","Sabato"],
+			monthNames:["Gen","Feb","Mar","Apr","Mag","Gui","Lug","Ago","Set","Ott","Nov","Dic","Genneio","Febbraio","Marzo","Aprile","Maggio","Giugno","Luglio","Agosto","Settembre","Ottobre","Movembre","Dicembre"],
+			AmPm:["am","pm","AM","PM"],
+			S:function(b){return b<11||b>13?["st","nd","rd","th"][Math.min((b-1)%10,3)]:"th"},
+			srcformat:"Y-m-d",
+			newformat:"d/m/Y",
 			parseRe : /[#%\\\/:_;.,\t\s-]/,
-			masks : {
-				// see http://php.net/manual/en/function.date.php for PHP format used in jqGrid
-				// and see http://docs.jquery.com/UI/Datepicker/formatDate
-				// and https://github.com/jquery/globalize#dates for alternative formats used frequently
-				// one can find on https://github.com/jquery/globalize/tree/master/lib/cultures many
-				// information about date, time, numbers and currency formats used in different countries
-				// one should just convert the information in PHP format
+			masks:{
 				ISO8601Long:"Y-m-d H:i:s",
-				ISO8601Short:"Y-m-d",
-				// short date:
-				//    n - Numeric representation of a month, without leading zeros
-				//    j - Day of the month without leading zeros
-				//    Y - A full numeric representation of a year, 4 digits
-				// example: 3/1/2012 which means 1 March 2012
-				ShortDate: "n/j/Y", // in jQuery UI Datepicker: "M/d/yyyy"
-				// long date:
-				//    l - A full textual representation of the day of the week
-				//    F - A full textual representation of a month
-				//    d - Day of the month, 2 digits with leading zeros
-				//    Y - A full numeric representation of a year, 4 digits
-				LongDate: "l, F d, Y", // in jQuery UI Datepicker: "dddd, MMMM dd, yyyy"
-				// long date with long time:
-				//    l - A full textual representation of the day of the week
-				//    F - A full textual representation of a month
-				//    d - Day of the month, 2 digits with leading zeros
-				//    Y - A full numeric representation of a year, 4 digits
-				//    g - 12-hour format of an hour without leading zeros
-				//    i - Minutes with leading zeros
-				//    s - Seconds, with leading zeros
-				//    A - Uppercase Ante meridiem and Post meridiem (AM or PM)
-				FullDateTime: "l, F d, Y g:i:s A", // in jQuery UI Datepicker: "dddd, MMMM dd, yyyy h:mm:ss tt"
-				// month day:
-				//    F - A full textual representation of a month
-				//    d - Day of the month, 2 digits with leading zeros
-				MonthDay: "F d", // in jQuery UI Datepicker: "MMMM dd"
-				// short time (without seconds)
-				//    g - 12-hour format of an hour without leading zeros
-				//    i - Minutes with leading zeros
-				//    A - Uppercase Ante meridiem and Post meridiem (AM or PM)
-				ShortTime: "g:i A", // in jQuery UI Datepicker: "h:mm tt"
-				// long time (with seconds)
-				//    g - 12-hour format of an hour without leading zeros
-				//    i - Minutes with leading zeros
-				//    s - Seconds, with leading zeros
-				//    A - Uppercase Ante meridiem and Post meridiem (AM or PM)
-				LongTime: "g:i:s A", // in jQuery UI Datepicker: "h:mm:ss tt"
-				SortableDateTime: "Y-m-d\\TH:i:s",
-				UniversalSortableDateTime: "Y-m-d H:i:sO",
-				// month with year
-				//    Y - A full numeric representation of a year, 4 digits
-				//    F - A full textual representation of a month
-				YearMonth: "F, Y" // in jQuery UI Datepicker: "MMMM, yyyy"
+				ISO8601Short:"Y-m-d", 
+				ShortDate:"d/m/Y", 
+				LongDate:"l d F Y",
+				FullDateTime:"l d F Y G:i:s",
+				MonthDay:"F d",
+				ShortTime:"H:i",
+				LongTime:"H:i:s",
+				SortableDateTime:"Y-m-d\\TH:i:s",
+				UniversalSortableDateTime:"Y-m-d H:i:sO",
+				YearMonth:"F, Y"
 			},
-			reformatAfterEdit : false,
+			reformatAfterEdit:false,
 			userLocalTime : false
 		},
-		baseLinkUrl: '',
-		showAction: '',
-		target: '',
-		checkbox : {disabled:true},
-		idName : 'id'
+		baseLinkUrl:"",
+		showAction:"",
+		target:"",
+		checkbox:{ disabled:true},
+		idName:"id"
 	}
 };
 }));
