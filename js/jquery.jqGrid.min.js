6915cf9141f48b83ef283c5ca8acf6f74771a30c
Tony Tomov <tony@trirand.com>
2016-02-07 16:41:11 +0200

    Update the build

--- js/jquery.jqGrid.min.js
+++ js/jquery.jqGrid.min.js
@@ -1,6 +1,6 @@
 /**
 *
-* @license Guriddo jqGrid JS - v5.0.2 - 2016-02-06
+* @license Guriddo jqGrid JS - v5.0.2 - 2016-02-07
 * Copyright(c) 2008, Tony Tomov, tony@trirand.com
 * 
 * License: http://guriddo.net/?page_id=103334
