3238d5a98b9d6d471e805c5eea34b492bd5f72d1
Tony Tomov <tony@trirand.com>
2016-04-25 15:51:02 +0300

    Fix jumping to the top of page when a colmenu is clicked.

--- js/grid.base.js
+++ js/grid.base.js
@@ -3497,7 +3497,7 @@ $.fn.jqGrid = function( pin ) {
 			}
 			thead += imgs;
 			if(ts.p.colMenu && tmpcm.colmenu) {
-				thead += "<a class='colmenu' href='#'><span class='colmenuspan "+iconbase+' '+colmenustyle.icon_menu+"'></span></a>";
+				thead += "<a class='colmenu' href='#/'><span class='colmenuspan "+iconbase+' '+colmenustyle.icon_menu+"'></span></a>";
 			}
 			thead += "</div></th>";
 		}
