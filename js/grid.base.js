fd1e65d76a63491cb5f01e43350d0f287e9848d6
Tony Tomov <tony@trirand.com>
2017-07-18 11:13:47 +0300

    Remove not needed href='#/' when define colmenu

--- js/grid.base.js
+++ js/grid.base.js
@@ -3610,7 +3610,7 @@ $.fn.jqGrid = function( pin ) {
 			}
 			thead += imgs;
 			if(ts.p.colMenu && tmpcm.colmenu) {
-				thead += "<a class='colmenu' href='#/'><span class='colmenuspan "+iconbase+' '+colmenustyle.icon_menu+"'></span></a>";
+				thead += "<a class='colmenu'><span class='colmenuspan "+iconbase+' '+colmenustyle.icon_menu+"'></span></a>";
 			}
 			thead += "</div></th>";
 		}
