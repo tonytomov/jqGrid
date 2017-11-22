364d028f2574bc6b1d5131f2cd22ef81baad89aa
Tony Tomov <tony@trirand.com>
2017-11-22 15:18:43 +0200

    Fix setCell to get right row unformated data. If the datatype is local no unformat function is needed

--- js/grid.base.js
+++ js/grid.base.js
@@ -5117,11 +5117,13 @@ $.jgrid.extend({
 					} catch(e){}
 					if(tcell) {
 						if(nData !== "" || forceupd === true ) {
-							if(ind.cells !== undefined) {
+							if($t.p.datatype === 'local') {
+								rawdat = $($t).jqGrid('getLocalRow', rowid);
+							} else if(ind.cells !== undefined) {
 								while(cl<ind.cells.length) {
 									// slow down speed
-									// rawdat should be unformated
-									rawdat.push(ind.cells[cl].innerHTML);
+									v = $.unformat.call($t,$(ind.cells[cl]),{rowId:ind.id, colModel:$t.p.colModel[cl]},cl);
+									rawdat.push(v);
 									cl++;
 								}
 							}
