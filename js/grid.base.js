8341ad2e1d7b92f3d52c66c8d013b681b19fdb3c
Tony Tomov <tony@trirand.com>
2018-05-02 12:12:08 +0300

    Fix preserveSelectio option when all rows are deleselected from header checkbox

--- js/grid.base.js
+++ js/grid.base.js
@@ -3482,7 +3482,7 @@ $.fn.jqGrid = function( pin ) {
 				ts.p.selrow = null;
 				if(ts.p.multiselect){
 					if(!ts.p.preserveSelection) {
-				ts.p.selarrrow =[];
+						ts.p.selarrrow =[];
 					}
 				}
 				ts.p.savedRow =[];
@@ -4147,7 +4147,7 @@ $.fn.jqGrid = function( pin ) {
 			var emp=[], chk;
 			$('#cb_'+$.jgrid.jqID(ts.p.id),this).on('click',function(){
 				if(!ts.p.preserveSelection) {
-				ts.p.selarrrow = [];
+					ts.p.selarrrow = [];
 				}
 				var froz = ts.p.frozenColumns === true ? ts.p.id + "_frozen" : "";
 				if (this.checked) {
@@ -4158,7 +4158,7 @@ $.fn.jqGrid = function( pin ) {
 								$(this).addClass(highlight).attr("aria-selected","true");
 								if(ts.p.preserveSelection) {
 									if(ts.p.selarrrow.indexOf(this.id) === -1) {
-								ts.p.selarrrow.push(this.id);
+										ts.p.selarrrow.push(this.id);
 									}
 								} else {
 									ts.p.selarrrow.push(this.id);
@@ -4181,6 +4181,12 @@ $.fn.jqGrid = function( pin ) {
 								$("#jqg_"+$.jgrid.jqID(ts.p.id)+"_"+$.jgrid.jqID(this.id) )[ts.p.useProp ? 'prop': 'attr']("checked", false);
 								$(this).removeClass(highlight).attr("aria-selected","false");
 								emp.push(this.id);
+								if(ts.p.preserveSelection) {
+									var curind = ts.p.selarrrow.indexOf(this.id);
+									if(curind > -1) {
+										ts.p.selarrrow.splice.push(curind, 1);
+									}
+								}
 								if(froz) {
 									$("#jqg_"+$.jgrid.jqID(ts.p.id)+"_"+$.jgrid.jqID(this.id), ts.grid.fbDiv )[ts.p.useProp ? 'prop': 'attr']("checked",false);
 									$("#"+$.jgrid.jqID(this.id), ts.grid.fbDiv).removeClass(highlight);
@@ -4523,9 +4529,9 @@ $.fn.jqGrid = function( pin ) {
 					ts.p.selrow=null;
 					if(ts.p.multiselect) {
 						if(!ts.p.preserveSelection) {
-						ts.p.selarrrow =[];
-						setHeadCheckBox(false);
-					}
+							ts.p.selarrrow =[];
+							setHeadCheckBox(false);
+						}
 					}
 					ts.p.savedRow = [];
 				}
