8d9f10aeb5a5f4622bf962c0f7027d7b44c61304
Tony Tomov <tony@trirand.com>
2020-12-07 12:38:35 +0200

    Fix multiselect plugin to work with jQuery 3.5.x

--- js/addons/ui.multiselect.js
+++ js/addons/ui.multiselect.js
@@ -163,7 +163,7 @@ $.widget("ui.multiselect", {
 	},
 	_getOptionNode: function(option) {
 		option = $(option);
-		var node = $('<li class="ui-state-default ui-element" title="'+option.text()+'"><span class="ui-icon"/>'+option.text()+'<a href="#" class="action"><span class="ui-corner-all ui-icon"/></a></li>').hide();
+		var node = $('<li class="ui-state-default ui-element" title="'+option.text()+'"><span class="ui-icon"></span>'+option.text()+'<a href="#" class="action"><span class="ui-corner-all ui-icon"></span></a></li>').hide();
 		node.data('optionLink', option);
 		return node;
 	},
