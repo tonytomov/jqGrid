396033980d7d3f1e3b2d03966d476de0b8f1c5ca
Tony Tomov <tony@trirand.com>
2022-02-28 12:34:48 +0200

    Moving some functions from transposed grid to base module

--- js/grid.base.js
+++ js/grid.base.js
@@ -7555,8 +7555,99 @@ $.jgrid.extend({
 		return this.each(function(){
 			$("#"+itemid, "#"+this.p.id+"_menubar").remove();
 		});
+	},
+	addColSearchMenu :  function() {
+		return this.each(function(){
+			var $t = this;
+			$($t).colMenuAdd('sc',{
+				title: 'Reset',
+				position : "first",
+				id :"reset",
+				funcname : function() {
+					$("input[id^='jqs_']", "#"+this.p.id ).each(function(i){
+						$(this).val("");
+					});
+					$(this)[0].p._results ={};
+					$(this).jqGrid('showCol', this.p._avc);
+				}
+			});
+			$($t).colMenuAdd('sc',{
+				separator : true,
+				id:"groupOp"
+			});
+			$($t).colMenuAdd('sc',{
+				separator : true,
+				id:"operOp"
+			});
+			$($t).colMenuAdd('sc',{
+				//title: 'Close',
+				separator : true,
+				id :"close",
+				funcname : function() {
+					return true;
+				}
+			});
+			$($t).on("jqGridcolMenuBeforeProcess.custom", function(t, p){
+				if(p.module === 'custom' && p.column === 'sc') {
+					var regional = $.jgrid.getRegional(this, "search"),
+						tstl,
+						sopt = this.p.searchColOptions,
+						getstyle = $.jgrid.getMethod("getStyleUI"),
+						stylemodule = this.p.styleUI + ".base";
+					if(p.action.id === "reset") {
+						p.action.title = regional.Reset || 'Reset';
+						p.action.icon = getstyle(this.p.styleUI + ".colmenu", 'icon_reset', true, '');
+					}
+					if(p.action.id === "close") {
+						p.action.title = regional.Close || 'Close';
+						p.action.icon = getstyle(this.p.styleUI + ".colmenu", 'icon_close', true, '');
+					}
+					if(p.action.id === "groupOp") {
+						tstl = getstyle(stylemodule, 'searchSelect', false, 'search-col-input');
+						p.action.title = ""+(regional.Operand || "Operand: ")+"";
+						p.action.title +='<select data-grid-id='+this.p.id+' id="opselect"  '+ tstl+'>';
+						tstl = getstyle(this.p.styleUI + ".colmenu", 'icon_group_op', true, ''); 
+						p.action.icon = tstl;
+						for (var i = 0;i< regional.groupOps.length; i++) {
+							var selected = sopt.searchOp === regional.groupOps[i].op ? "selected" : "";
+							p.action.title += "<option value=\"" + regional.groupOps[i].op +"\" " + selected+">"+regional.groupOps[i].text+"</option>";
+						}
+						p.action.title += "</select>";
+						setTimeout(function() {
+							$("#opselect").on('change', function(){
+								var gid =$("#"+ $(this).attr('data-grid-id'))[0];
+								gid.p.searchColOptions.searchOp = $(this).val();
+								$.jgrid.myfunc(gid.p.id);
+								//$("#column_menu").remove();
+							});
+						}, 200);
+					}
+					if(p.action.id === "operOp") {
+						tstl = getstyle(stylemodule, 'searchSelect', false, 'search-col-input');
+						p.action.title = regional.Operation || "Oper : ";
+						p.action.title += '<select data-grid-id='+this.p.id+' id="selectoper"  '+ tstl+'>';
+						tstl = getstyle(this.p.styleUI + ".colmenu", 'icon_oper_op', true, ''); 
+						p.action.icon = tstl;
+						for (var i = 0;i <  regional.odata.length; i++) {
+							var selected = sopt.operand === regional.odata[i].oper ? "selected" : "";
+							if(sopt.aOperands.includes( regional.odata[i].oper )) {
+								p.action.title += "<option value=\"" + regional.odata[i].oper +"\" " + selected+">"+regional.odata[i].text+"</option>";
+							}
+						}
+						p.action.title += "</select>";
+						setTimeout(function() {
+							$("#selectoper").on('change', function(){
+								var gid =$("#"+ $(this).attr('data-grid-id'))[0];
+								gid.p.searchColOptions.operand = $(this).val();
+								$.jgrid.myfunc(gid.p.id);
+								//$("#column_menu").remove();
+							});
+						}, 200);
+					}
+				}
+			});
+		});
 	}
-
 });
 //module end
 }));
