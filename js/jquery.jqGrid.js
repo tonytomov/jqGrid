646a44ea947e792b7970eb407f3b37163d53e4d1
Tony Tomov <tony@trirand.com>
2022-05-17 17:52:29 +0300

    New build

--- js/jquery.jqGrid.js
+++ js/jquery.jqGrid.js
@@ -1,6 +1,6 @@
 /**
 *
-* @license Guriddo jqGrid JS - v5.7.0 - 2022-05-11
+* @license Guriddo jqGrid JS - v5.7.0 - 2022-05-17
 * Copyright(c) 2008, Tony Tomov, tony@trirand.com
 * 
 * License: http://guriddo.net/?page_id=103334
