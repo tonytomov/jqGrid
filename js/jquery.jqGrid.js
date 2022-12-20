94a1d361c5cea932b51f4b210cbac7be40f05829
Tony Tomov <tony@trirand.com>
2022-12-20 19:22:14 +0200

    Rebuild the lib

--- js/jquery.jqGrid.js
+++ js/jquery.jqGrid.js
@@ -2295,7 +2295,7 @@ $.fn.jqGrid = function( pin ) {
 			colMenuCustom : {},
 			colMenuColumnDone : null,
 			colMenuBeforeProcess : null,
-			colSpanHeader: [],
+			colSpanHeader : [],
 			// tree pagging
 			treeGrid_bigData: false,
 			treeGrid_rootParams: {otherData:{}},
