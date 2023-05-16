fd2680e2196d351fed2f72bd2a2e5a7e8923d1f5
asaguer <ali.saguer@softeam.fr>
2023-05-16 16:33:56 +0100

    Use concrete listbox role instead of abstract select role

--- js/grid.common.js
+++ js/grid.common.js
@@ -405,7 +405,7 @@ $.extend($.jgrid,{
 			break;
 			case "select" :
 				elem = document.createElement("select");
-				elem.setAttribute("role","select");
+				elem.setAttribute("role","listbox");
 				var msl, ovm = [];
 				if(options.multiple===true) {
 					msl = true;
