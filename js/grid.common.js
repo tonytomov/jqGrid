6fcb8fb0ec5509ad9bd1196fc01354d1977e4c66
Tony Tomov <tony@trirand.com>
2023-12-13 12:43:59 +0200

    Add postForm function to post data to server without ajax

--- js/grid.common.js
+++ js/grid.common.js
@@ -928,9 +928,31 @@ $.extend($.jgrid,{
 			if($t.p.colModel[i].hidden !== true ) {
 				ret = i;
 				break;
-	}	
+			}	
 		}
 		return ret;
+	},
+	/* post data to server get or post without ajax */
+	postForm : function (path, params, method) {
+		method = method || 'post';
+		
+	    var form = document.createElement('form');
+		form.setAttribute('method', method);
+		form.setAttribute('action', path);
+		for (var key in params) {
+			if (params.hasOwnProperty(key)) {
+				var hiddenField = document.createElement('input');
+				hiddenField.setAttribute('type', 'hidden');
+				hiddenField.setAttribute('name', key);
+				hiddenField.setAttribute('value', params[key]);
+
+				form.appendChild(hiddenField);
+			}
+		}
+
+		document.body.appendChild(form);
+		form.submit();
+		form.parentNode.removeChild(form);
 	}	
 });
 //module end
